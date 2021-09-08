3ecbaf7e28737a33c443f97f45dab11d6a16b18b
CarlosOctavioHA <88982817+CarlosOctavioHA@users.noreply.github.com>
2021-09-07 22:39:54 -0500

    Add files via upload

---
+++ tarea-4-CarlosOctavioHA/assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -0,0 +1,17 @@
+#Alternar caracteres 
+
+num = int(input("Dame un numero"))
+cont = 0
+variable_1= "#"
+
+while cont < num:
+
+    cont = cont + 1
+
+    if variable_1 == "#":
+        print(cont,"-",  variable_1)
+        variable_1= "%"
+    else:
+        variable_1 == "%"
+        print (cont,"-", variable_1)
+        variable_1= "#"
